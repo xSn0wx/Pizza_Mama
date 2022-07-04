57da5443dfa6c47b957b7ed116da364f6f8fb581
Snow <104694981+xSn0wx@users.noreply.github.com>
2022-07-04 16:32:29 +0200

    Add files via upload

---
+++ pizzamama/menu/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pizza
+# Create your views here.
+
+def index(requests):
+    pizzas = Pizza.objects.all()
+    pizza_names_and_price = [pizza.nom + " : " + str(pizza.prix) + "€" for pizza in pizzas]
+    pizza_names_and_prices_str = ", ".join(pizza_names_and_price)
+    return HttpResponse("Les pizzas : " + pizza_names_and_prices_str)
