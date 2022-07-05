b93aac4bc379e43ec080b845b1d71d8e75202dae
Snow <104694981+xSn0wx@users.noreply.github.com>
2022-07-05 16:16:04 +0200

    Add files via upload

--- pizzamama/main/urls.py
+++ pizzamama/main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'menu'
+app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 from .models import Pizza
 # Create your views here.
 
-def index(requests):
-    pizzas = Pizza.objects.all()
+def index(request):
+    '''pizzas = Pizza.objects.all()
     pizza_names_and_price = [pizza.nom + " : " + str(pizza.prix) + "€" for pizza in pizzas]
     pizza_names_and_prices_str = ", ".join(pizza_names_and_price)
-    return HttpResponse("Les pizzas : " + pizza_names_and_prices_str)
+    return HttpResponse("Les pizzas : " + pizza_names_and_prices_str)'''
+    pizzas = Pizza.objects.all().order_by('prix')
+    return render(request, 'menu/index.html', {'pizzas' : pizzas})
