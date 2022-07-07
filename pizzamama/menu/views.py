b84e31adece26f602b3069bd01eef3cbff3bbb5e
Snow <104694981+xSn0wx@users.noreply.github.com>
2022-07-07 12:57:46 +0200

    Add files via upload

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
 from .models import Pizza
 # Create your views here.
 
@@ -10,3 +11,8 @@ def index(request):
     return HttpResponse("Les pizzas : " + pizza_names_and_prices_str)'''
     pizzas = Pizza.objects.all().order_by('prix')
     return render(request, 'menu/index.html', {'pizzas' : pizzas})
+
+def api_get_pizzas(requests):
+    pizzas = Pizza.objects.all().order_by('prix')
+    json = serializers.serialize("json", pizzas)
+    return HttpResponse(json)
--- pizzamama/pizzamama/settings.py
+++ pizzamama/pizzamama/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-e!qouvo*=n@xycg3u(**1!^^-y-ifb@p!7)qidtnp6ocw@tna+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sn0w667.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
@@ -124,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
